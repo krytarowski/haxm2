4bb90c4c08615e75d315f19cf0a306cc2807b0e9
Kamil Rytarowski <n54@gmx.com>
2018-12-10 06:57:31 +0100

    Introduce max_cpus
    Initialize max_cpus to 0 in module initialization.

---
+++ include/hax.h
@@ -0,0 +1,6 @@
+#ifndef HAX_H_
+#define HAX_H_
+
+extern int max_cpus;
+
+#endif  // HAX_H_
--- platforms/netbsd/hax_entry.c
+++ platforms/netbsd/hax_entry.c
@@ -2,14 +2,19 @@
 #include <sys/types.h>
 #include <sys/module.h>
 
+#include "../../include/hax.h"
+
 MODULE(MODULE_CLASS_MISC, haxm, NULL);
 
 static int
 haxm_modcmd(modcmd_t cmd, void *arg __unused)
 {
 	switch (cmd) {
-	case MODULE_CMD_INIT:
+	case MODULE_CMD_INIT: {
+		// Initialization
+		max_cpus = 0;
 		return 0;
+	}
 	case MODULE_CMD_FINI:
 		return 0;
 	default:
