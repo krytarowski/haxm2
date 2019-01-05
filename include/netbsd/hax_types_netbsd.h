a04bca22af7d5d22b96de4d4680737aa5c4a0aa8
Kamil Rytarowski <n54@gmx.com>
2019-01-05 07:34:11 +0100

    Define basic int/uint typedefs

--- include/netbsd/hax_types_netbsd.h
+++ include/netbsd/hax_types_netbsd.h
@@ -1,6 +1,25 @@
 #ifndef HAX_NETBSD_HAX_TYPES_NETBSD_H_
 #define HAX_NETBSD_HAX_TYPES_NETBSD_H_
 
+#include <sys/param.h>
+#include <sys/types.h>
+
+// Signed Types
+typedef int8_t  int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+// Unsigned Types
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+typedef unsigned int  uint;
+typedef unsigned long ulong;
+typedef unsigned long ulong_t;
+
 typedef int hax_cpumap_t;
 
 #endif  // HAX_NETBSD_HAX_TYPES_NETBSD_H_
