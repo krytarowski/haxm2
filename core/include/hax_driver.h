0d6162c2739c231d826a1892d66d56166dcc8b96
Kamil Rytarowski <n54@gmx.com>
2019-01-06 04:01:40 +0100

    Add apm fields in hax_t

--- core/include/hax_driver.h
+++ core/include/hax_driver.h
@@ -6,6 +6,14 @@ struct hax_t {
 	int nx_enable_flag;
 	int em64t_enable_flag;
 	int ug_enable_flag;
+
+	uint apm_version;
+	uint apm_general_count;
+	uint64_t apm_general_mask;
+	uint apm_event_count;
+	uint32_t apm_event_unavailability;
+	uint apm_fixed_count;
+	uint64_t apm_fixed_mask;
 };
 
 #endif // HAX_CORE_HAX_DRIVER_H_
