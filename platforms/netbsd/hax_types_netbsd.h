60165d73d3b761589826732014e25f9bc877adc9
Kamil Rytarowski <n54@gmx.com>
2019-01-09 04:21:53 +0100

    Add basic hax_types_netbsd.h with typedefs

---
+++ platforms/netbsd/hax_types_netbsd.h
@@ -0,0 +1,30 @@
+#ifndef HAX_NETBSD_HAX_TYPES_NETBSD_H_
+#define HAX_NETBSD_HAX_TYPES_NETBSD_H_
+
+#include <sys/param.h>
+#include <sys/types.h>
+
+typedef	int8_t		int8;
+typedef	int16_t		int16;
+typedef	int32_t		int32;
+typedef int64_t		int64;
+
+typedef	uint8_t		uint8;
+typedef uint16_t	uint16;
+typedef	uint32_t	uint32;
+typedef	uint64_t	uint64;
+
+typedef	unsigned int	uint;
+typedef unsigned long	ulong;
+typedef unsigned long	ulong_t;
+
+#if defined(__i386__)
+typedef uint32_t mword;
+#endif
+#if defined(__x86_64__)
+typedef uint64_t mword;
+#endif
+
+typedef mword	HAX_VADDR_T;
+
+#endif // HAX_NETBSD_HAX_TYPES_NETBSD_H_
