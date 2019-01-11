eaa5a88983d30868c38fd200ee4a8708af91919a
Kamil Rytarowski <n54@gmx.com>
2019-01-11 05:46:34 +0100

    Set hax->mem_quota and hax->mem_limit in hax_module_init()

--- core/hax.c
+++ core/hax.c
@@ -10,6 +10,8 @@ int hax_module_init(void)
 	hax = (struct hax_t *)hax_vmalloc(sizeof(struct hax_t), HAX_MEM_NONPAGE);
 	if (!hax)
 		return -ENOMEM;
+
+	hax->mem_quota = hax->mem_limit = hax_get_memory_threshold();
 }
 
 int hax_module_exit(void)
