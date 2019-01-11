59b5fff5826f81925d41b21e3500e1353f863cde
Kamil Rytarowski <n54@gmx.com>
2019-01-11 06:23:20 +0100

    Initialize hax->hax_lock in hax_module_init()

--- core/hax.c
+++ core/hax.c
@@ -12,6 +12,14 @@ int hax_module_init(void)
 		return -ENOMEM;
 
 	hax->mem_quota = hax->mem_limit = hax_get_memory_threshold();
+	hax->hax_lock = hax_mutex_alloc_init();
+	if (!hax->hax_lock)
+		goto out_0;
+
+	return 0;
+out_0:
+	hax_vfree(hax, sizeof(struct hax_t));
+	return -ENOMEM;
 }
 
 int hax_module_exit(void)
